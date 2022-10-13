13c72cfc3c293f55a8025f1e6b7674bf399d3025
rexoplans1 <yigitkuru13@gmail.com>
2022-10-13 12:04:28 +0300

    Full algorithm before testing
    The algorithm was designed and decoded. Added functions for testing. It is awaiting testing.

--- Project_2/Arduino/UNO_project_2/UNO_project_2.ino
+++ Project_2/Arduino/UNO_project_2/UNO_project_2.ino
@@ -17,6 +17,7 @@ Bu 2 task birbirini bloke etmeyecek.
 String get_data;
 unsigned long old_time=0;
 unsigned long max_time=0,on_time=0,off_time=0; //max_tim=on_time+off_time
+int echo_mode=0;
 void setup() {
   // put your setup code here, to run once:
   
@@ -55,6 +56,46 @@ void loop() {
     get_data=Serial.readString();
     //For test 
     Serial.println(get_data);
+      if(get_data=="start")
+    {
+      echo_mode=1;
+    }
+    else if(get_data=="stop")
+    {
+      echo_mode=0;
+      unsigned long stop_off_time=1000;
+      unsigned long stop_on_time=1000;
+      max_time=stop_on_time+stop_off_time;
+      Serial.println(max_time);
+    }
+    if(echo_mode==1)
+    {
+      Serial.println(get_data);
+      max_time=on_time+off_time;
+      int index_string=get_data.length();
+      Serial.println(index_string); //For Testing
+      String check_on_string = get_data.substring(0,6); //"ledon="
+      Serial.println(check_on_string); //For Testing
+      String check_off_string =get_data.substring(0,7); //"ledoff="
+      Serial.println(check_off_string); //For Testing
+      if(check_on_string=="ledon=")
+      {
+        String timeon_value=get_data.substring(6,index_string);
+        on_time=atol(timeon_value.c_str());
+        Serial.println(on_time);
+        max_time=on_time+off_time;
+        Serial.println(max_time);
+        
+      }
+      if(check_off_string=="ledoff=")
+      {
+        String timeoff_value=get_data.substring(7,index_string);
+        off_time=atol(timeoff_value.c_str());
+        Serial.println(off_time);
+        max_time=on_time+off_time;
+        Serial.println(max_time);
+      }   
+    }
   }
 
 }
